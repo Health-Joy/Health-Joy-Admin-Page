fbaae4c14e3adb1bb997360951ad63b95ae52bbe
fatiihvarol <fatihvrl38@gmail.com>
2024-05-01 20:17:37 +0300

    Login fixes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,38 +11,40 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const data = await loginUser(email, password); // loginUser fonksiyonunu çağır
-      console.log("Login successful!", data); // API'den dönen veriyi konsola yazdır
-      // Başarılı giriş durumunda ekran üzerinde işlem yapabilirsiniz
+      const data = await loginUser(email, password);
+      console.log("Login successful!", data);
+      // Perform actions upon successful login
     } catch (error) {
       console.error("Login failed!", error.message);
-      setLoginError("Kullanıcı adı veya şifre hatalı."); // Hata durumunda kullanıcıya bilgi ver
+      setLoginError("Kullanıcı adı veya şifre hatalı.");
     }
   };
 
   return (
     <div>
-      <h2>Login</h2>
+      <h1>ADMIN PANEL</h1>
+      <h4>Health & Joy</h4>
+
       <form onSubmit={handleLogin}>
         <label>
-          Kullanıcı Adı:
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
           />
         </label>
         <br />
         <label>
-          Şifre:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
           />
         </label>
         <br />
-        <button type="submit">Giriş Yap</button>
+        <button type="submit">Login</button>
         {loginError && <p style={{ color: "red" }}>{loginError}</p>}
       </form>
     </div>
